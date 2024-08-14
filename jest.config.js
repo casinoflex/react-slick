1163402a0ee76734675d640afa94e85c741773b2
afc163 <afc163@gmail.com>
2024-08-14 14:46:55 +0800

    test: Update jest.config.js (#116)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   collectCoverage: true,
   coverageReporters: ['text', 'cobertura'],
+  collectCoverageFrom: [
+    'src/**/*',
+  ],
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/test-setup.js"],
   testPathIgnorePatterns: ["/node_modules/", "e2e-tests"],
